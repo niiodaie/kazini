2f60c6edcb2a38b34acdbc21231c0bab73243591
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-28 11:17:00 -0500

    Create plans.js

---
+++ src/plans.js
@@ -0,0 +1,27 @@
+// src/plans.js
+export const PLANS = {
+  pro_monthly: {
+    id: '047ff453-e2b2-4071-a3c5-75877848d213',
+    name: 'Kazini Pro – Monthly',
+    price: '$9.99/month',
+    link: 'https://visnec.lemonsqueezy.com/buy/047ff453-e2b2-4071-a3c5-75877848d213',
+  },
+  pro_yearly: {
+    id: '418427af-f316-480b-9d8b-a16e40a3ee5c',
+    name: 'Kazini Pro – Yearly',
+    price: '$99.00/year',
+    link: 'https://visnec.lemonsqueezy.com/buy/418427af-f316-480b-9d8b-a16e40a3ee5c',
+  },
+  enterprise_monthly: {
+    id: '2c55c026-f136-4f19-9492-a178b7c5b5a0',
+    name: 'Kazini Enterprise – Monthly',
+    price: '$49.99/month',
+    link: 'https://visnec.lemonsqueezy.com/buy/2c55c026-f136-4f19-9492-a178b7c5b5a0',
+  },
+  enterprise_yearly: {
+    id: '3d68f498-3793-4a5c-be22-62d0dfe94376',
+    name: 'Kazini Enterprise – Yearly',
+    price: '$499.00/year',
+    link: 'https://visnec.lemonsqueezy.com/buy/3d68f498-3793-4a5c-be22-62d0dfe94376',
+  },
+};
