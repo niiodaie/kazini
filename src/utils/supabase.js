87c0e2a32242771c05a15c2857c7fcc0cd6113a5
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-30 09:51:12 -0500

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,5 +1,5 @@
 // src/utils/supabase.js
-import { createClient } from '@supabase/supabase-js';
+import { supabase } from './supabase';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
