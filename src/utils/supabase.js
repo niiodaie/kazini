003f787051841a3811748ca24f712ed315b454cb
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-07-30 09:44:04 -0500

    Update and rename supabase.jsx to supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,5 +1,5 @@
-// src/utils/supabaseClient.js
-import { createClient } from '@supabase/supabase-jsx';
+// src/utils/supabase.js
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
