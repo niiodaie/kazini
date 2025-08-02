b596a09e053c30e1a372b5b882fac8c887fe3d29
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-08-02 02:24:46 -0500

    Update authUtils.js

--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -66,7 +66,6 @@ export const handleEmailSignup = async (email, password, metadata = {}) => {
       email,
       password,
       options: {
-        //emailRedirectTo: `${window.location.origin}/auth-handler`,
         data: {
           display_name: metadata.displayName || email.split('@')[0],
           ...metadata,
